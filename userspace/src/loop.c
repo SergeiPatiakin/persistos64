757d43a9f1957c8fb3a659cedaae9b498510d445
Sergei Patiakin <sergei.patiakin@gmail.com>
2025-04-28 21:32:01 +0200

    Loop options

--- userspace/src/loop.c
+++ userspace/src/loop.c
@@ -3,8 +3,19 @@
 #include "cstd.h"
 #include <persistos.h>
 
-void main() {
-    while (true) {
-        sched_yield();
+void main(int argc, char* argv[]) {
+    if (argc < 2) {
+        fputs("Expected a loop type argument\n", stderr);
+        exit(1);
+    }
+    if (strcmp(argv[1], "busy") == 0) {
+        while (true);
+    } else if (strcmp(argv[1], "yield") == 0) {
+        while (true) sched_yield();
+    } else if (strcmp(argv[1], "sleep") == 0) {
+        while (true) pause();
+    } else {
+        fputs("Unknown loop type\n", stderr);
+        exit(1);
     }
 }
