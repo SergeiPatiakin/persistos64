ec5b003cd9f92c5095e2e47d729a1d9920f10d2a
Sergei Patiakin <sergei.patiakin@gmail.com>
2025-05-11 21:39:30 +0100

    Simplify test setup

---
+++ userspace/test/run.sh
@@ -0,0 +1,75 @@
+set -e
+
+echo test_mkdir_basic
+mkdir td
+
+echo test_mkdir_subdir
+mkdir td/subdir
+
+echo test_mkdir_exists
+set +e
+mkdir td/subdir 2>td/mkdir_exists.err
+assert_exit_code 2
+set -e
+diff td/mkdir_exists.err test/mkdir_exists.err.exp
+
+echo test_touch_basic
+touch td/touch_basic.out
+diff td/touch_basic.out test/touch_basic.out.exp
+
+echo test_diff_length
+set +e
+diff test/diff_a.txt test/diff_aa.txt > td/diff_length.out
+assert_exit_code 2
+set -e
+diff td/diff_length.out test/diff_length.out.exp
+
+echo test_diff_differs
+set +e
+diff test/diff_a.txt test/diff_b.txt > td/diff_differs.out
+assert_exit_code 2
+set -e
+diff td/diff_differs.out test/diff_differs.out.exp
+
+echo test_echo_existing
+echo aaa >td/echo_existing.out
+echo aa >td/echo_existing.out
+diff td/echo_existing.out test/echo_existing.out.exp
+
+echo test_echo_new
+echo aa > td/echo_new.out
+
+echo test_cat_basic
+cat test/cat_basic_a.txt test/cat_basic_b.txt > td/cat_basic.out
+diff td/cat_basic.out test/cat_basic.out.exp
+
+echo test_shell_single_quotes
+e'ch'o h'el'lo '  world' > td/shell_single_quotes.out
+diff td/shell_single_quotes.out test/shell_single_quotes.out.exp
+
+echo test_hd_basic
+hd test/hd_basic.txt > td/hd_basic.out
+diff td/hd_basic.out test/hd_basic.out.exp
+
+echo test_ls_basic
+mkdir td/ls_basic
+touch td/ls_basic/a
+touch td/ls_basic/b
+ls td/ls_basic > td/ls_basic.out
+diff td/ls_basic.out test/ls_basic.out.exp
+
+echo test_truncate_basic
+echo aaa > td/truncate_basic.out
+truncate td/truncate_basic.out
+diff td/touch_basic.out test/truncate_basic.out.exp
+
+echo test_sysfs_pciinfo
+cat /sys/pciinfo > td/sysfs_pciinfo.out
+
+echo test_sysfs_meminfo
+cat /sys/meminfo > td/sysfs_meminfo.out
+
+echo test_sysfs_nvme
+cat /sys/nvme > td/sysfs_nvme.out
+
+echo All tests successful
