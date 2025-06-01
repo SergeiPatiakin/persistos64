dde8f657f345aeba28d60b8d6166c1806fcd2184
Sergei Patiakin <sergei.patiakin@gmail.com>
2025-06-01 21:43:51 +0200

    iview

---
+++ userspace/src/iview.c
@@ -0,0 +1,96 @@
+#include <stdint.h>
+#include "cstd.h"
+#include <persistos.h>
+
+struct bmp_file_header {
+	uint8_t ident[2];
+	uint32_t file_size;
+	uint8_t reserve_1[2];
+	uint8_t reserve_2[2];
+	uint32_t pix_array_offset;
+} __attribute__((packed));
+
+struct bmp_bitmap_info {
+	uint32_t size;
+	uint32_t width;
+	uint32_t height;
+	uint16_t colour_plain;
+	uint16_t bbp;
+	char compression_method[4];
+	char img_size[4];
+	char horizontal_res[4];
+	char vertical_res[4];
+	char num_colours[4];
+	char important_colours[4];
+} __attribute__((packed));
+
+void main(int argc, uint8_t* argv[]) {
+    if (argc < 2) {
+        fputs(u8p("iview: expected a filepath argument\n"), stderr);
+        exit(1);
+    }
+    if (argc > 2) {
+        fputs(u8p("iview: unexpected argument\n"), stderr);
+        exit(1);
+    }
+    ssize_t img_fd = open(argv[1], 0);
+    if (is_error(img_fd)) {
+        fputs(u8p("iview: error opening file\n"), stderr);
+        exit(1);
+    }
+    ssize_t fb_fd = open("/dev/fb0", 0);
+    if (is_error(fb_fd)) {
+        fputs(u8p("iview: error opening framebuffer\n"), stderr);
+        exit(1);
+    }
+    struct bmp_file_header file_header_buffer;
+    read(img_fd, &file_header_buffer, sizeof(struct bmp_file_header));
+    if (file_header_buffer.ident[0] != 'B' || file_header_buffer.ident[1] != 'M') {
+        fputs(u8p("iview: not a BMP file\n"), stderr);
+        exit(1);
+    }
+    struct bmp_bitmap_info bitmap_info_buffer;
+    read(img_fd, &bitmap_info_buffer, sizeof(struct bmp_bitmap_info));
+    if (bitmap_info_buffer.bbp != 24) {
+        fputs(u8p("iview: only 24-bit bitmaps supported\n"), stderr);
+        exit(1);
+    }
+
+    // char buf[17];
+    // sprintf_dec(bitmap_info_buffer.width, &buf, 0, 0);
+    // puts(buf);
+    // puts("x");
+    // sprintf_dec(bitmap_info_buffer.height, &buf, 0, 0);
+    // puts(buf);
+    // puts(", bbp=");
+    // sprintf_dec(bitmap_info_buffer.bbp, &buf, 0, 0);
+    // puts(buf);
+    // puts("\n");
+
+    size_t fb_width = 800; // 800 TODO
+    size_t fb_height = 600; // 600 TODO
+    size_t fb_pitch = 0xc80; // TODO
+
+    size_t width_to_display = fb_width < bitmap_info_buffer.width ? fb_width : bitmap_info_buffer.width;
+    size_t height_to_display = fb_height < bitmap_info_buffer.height ? fb_height : bitmap_info_buffer.height;
+
+    uint8_t *img_pixels_buffer = malloc(width_to_display * 3);
+    uint8_t *fb_pixels_buffer = malloc(width_to_display * 4);
+    for (int i = 0; i < height_to_display; i++) {
+        lseek(
+            img_fd,
+            file_header_buffer.pix_array_offset + (bitmap_info_buffer.height - 1 - i) * (bitmap_info_buffer.width * 3),
+            0
+        );
+        read(img_fd, img_pixels_buffer, width_to_display * 3);
+        for (int j = 0; j < width_to_display; j++) {
+            fb_pixels_buffer[4 * j] = img_pixels_buffer[3 * j]; // B
+            fb_pixels_buffer[4 * j + 1] = img_pixels_buffer[3 * j + 1]; // G
+            fb_pixels_buffer[4 * j + 2] = img_pixels_buffer[3 * j + 2]; // R
+            fb_pixels_buffer[4 * j + 3] = 0; // Alpha
+        }
+        write(fb_fd, fb_pixels_buffer, width_to_display * 4);
+        lseek(fb_fd, fb_pitch * (i + 1), 0);
+    }
+    exit(0);
+}
