f9f4b250d63ccf72e4ce13b2ceec60e004b2fb00
Sergei Patiakin <sergei.patiakin@gmail.com>
2025-06-16 19:54:42 +0200

    Display small images

--- userspace/src/iview.c
+++ userspace/src/iview.c
@@ -63,29 +63,45 @@ void main(int argc, uint8_t* argv[]) {
         exit(1);
     }
 
-    size_t fb_width = fb_info.fb_width;
-    size_t fb_height = fb_info.fb_height;
-    size_t fb_pitch = fb_info.fb_pitch;
+    int fb_width = fb_info.fb_width;
+    int fb_height = fb_info.fb_height;
+    int fb_pitch = fb_info.fb_pitch;
 
-    int width_to_display = fb_width < bitmap_info_buffer.width ? fb_width : bitmap_info_buffer.width;
-    int height_to_display = fb_height < bitmap_info_buffer.height ? fb_height : bitmap_info_buffer.height;
+    int width_to_display = fb_width < (int)bitmap_info_buffer.width ? fb_width : bitmap_info_buffer.width;
+    int height_to_display = fb_height < (int)bitmap_info_buffer.height ? fb_height : bitmap_info_buffer.height;
+    int left_margin = (fb_width - width_to_display) / 2;
+    int top_margin = (fb_height - height_to_display) / 2;
 
     uint8_t *img_pixels_buffer = malloc(width_to_display * 3);
-    uint8_t *fb_pixels_buffer = malloc(width_to_display * 4);
-    for (int i = 0; i < height_to_display; i++) {
-        lseek(
-            img_fd,
-            file_header_buffer.pix_array_offset + (bitmap_info_buffer.height - 1 - i) * (bitmap_info_buffer.width * 3),
-            SEEK_SET
-        );
-        read(img_fd, img_pixels_buffer, width_to_display * 3);
-        for (int j = 0; j < width_to_display; j++) {
-            fb_pixels_buffer[4 * j] = img_pixels_buffer[3 * j]; // B
-            fb_pixels_buffer[4 * j + 1] = img_pixels_buffer[3 * j + 1]; // G
-            fb_pixels_buffer[4 * j + 2] = img_pixels_buffer[3 * j + 2]; // R
-            fb_pixels_buffer[4 * j + 3] = 0; // Alpha
+    uint8_t *fb_pixels_buffer = malloc(fb_width * 4);
+    for (int i = 0; i < fb_height; i++) {
+        if ((i < top_margin) || (i > top_margin + height_to_display - 1)) {
+            memset(fb_pixels_buffer, 0, 4 * fb_width);
+        } else {
+            int bitmap_top = i - top_margin;
+            lseek(
+                img_fd,
+                file_header_buffer.pix_array_offset + (bitmap_info_buffer.height - 1 - bitmap_top) * (bitmap_info_buffer.width * 3),
+                SEEK_SET
+            );
+            read(img_fd, img_pixels_buffer, width_to_display * 3);
+            for (int j = 0; j < fb_width; j++) {
+                uint8_t red, green, blue;
+                if ((j < left_margin) || (j > left_margin + width_to_display - 1)) {
+                    red = green = blue = 0;
+                } else {
+                    int bitmap_left = j - left_margin;
+                    blue = img_pixels_buffer[3 * bitmap_left]; // B
+                    green = img_pixels_buffer[3 * bitmap_left + 1]; // G
+                    red = img_pixels_buffer[3 * bitmap_left + 2]; // R
+                }
+                fb_pixels_buffer[4 * j] = blue;
+                fb_pixels_buffer[4 * j + 1] = green;
+                fb_pixels_buffer[4 * j + 2] = red;
+                fb_pixels_buffer[4 * j + 3] = 0; // Alpha
+            }
         }
-        write(fb_fd, fb_pixels_buffer, width_to_display * 4);
+        write(fb_fd, fb_pixels_buffer, fb_width * 4);
         lseek(fb_fd, fb_pitch * (i + 1), SEEK_SET);
     }
     uint8_t buf[1];
